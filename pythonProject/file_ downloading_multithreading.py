a6a6c372110a1d99aff46f26d9149e80a18b7f31
Rishu Singh <147025219+rsRishuSingh@users.noreply.github.com>
2025-01-22 14:27:35 +0530

    Update file_ downloading_multithreading.py

--- pythonProject/file_ downloading_multithreading.py
+++ pythonProject/file_ downloading_multithreading.py
@@ -1,239 +1,188 @@
-# Date 13th October 2023
-
-import requests,threading,time
-import os,shutil
-import pyttsx3,speech_recognition as sr
+# Required imports for the script functionality
+import requests, threading, time
+import os, shutil
+import pyttsx3, speech_recognition as sr
 from concurrent.futures import ThreadPoolExecutor as tpe
 
+# Initializing the speech recognizer and text-to-speech engine
+r = sr.Recognizer()  # Recognizer object for speech recognition
+engine = pyttsx3.init()  # Text-to-speech engine initialization
+number = 0
 
+""" Voice Configuration """
+# Setting up the voice properties for text-to-speech
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)  # Using the female voice
 
+""" Rate Configuration """
+rate = engine.getProperty('rate')  # Retrieving current speaking rate
+print(rate)  # Displaying current rate
+engine.setProperty('rate', 150)  # Adjusting to a slower speaking rate
 
-r=sr.Recognizer()                   # initialiser of recognizer
-engine= pyttsx3.init()              # initialiser of engine
-number=0
+# Function to download and save an image
+def ImageDownloader(obj, n):
+    if not os.path.exists("DeleteME"):  # Check if "DeleteME" folder exists
+        os.mkdir("DeleteME")  # Create folder if not exists
 
-"""VOICE"""
-voices = engine.getProperty('voices')       #getting details of current voice
-#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-engine.setProperty('voice', voices[1].id)
-
-""" RATE"""
-rate = engine.getProperty('rate')   # getting details of current speaking rate
-print (rate)                        #printing current voice rate
-engine.setProperty('rate', 150)     # setting up new voice rate
-
-def ImageDownloader(obj,n):
-    
-    if(not os.path.exists("DeleteME")):         #folder is created if it does not exist
-        os.mkdir("DeleteME")
-  
-    print(f"we are starting download {n}")
-    open(f'DeleteME/image{n}.jpg','wb').write(obj.content)  # writing the file
-    #str1=f'image{n} downloaded'
-    #engine.say(str1)
-    #engine.runAndWait()  # they will become bottle's neck in multiprocessing
+    print(f"We are starting download {n}")
+    open(f'DeleteME/image{n}.jpg', 'wb').write(obj.content)  # Save the image
     print(f"Downloading {n} Ended")
 
-
+# Function to manage image downloading
 def Image_meta(num):
-   
-    engine.say(str1:="image download started")
-    engine.runAndWait()             #stopping until speech synthesis ends
-    
-    list_of_started_tasks=list()  # to be used below
-    
-    t1=time.perf_counter()
+    engine.say(str1 := "Image download started")
+    engine.runAndWait()  # Wait for speech synthesis to complete
 
-    for i,j in enumerate(range(num),start=1):
+    list_of_started_tasks = []  # List to keep track of threads
 
-        image_obj=requests.get("https://loremflickr.com/3200/2400")         # getting image by request
-        
-        task=threading.Thread(target=ImageDownloader,args=[image_obj,i])
-        task.start()
-        list_of_started_tasks.append(task)
-    
-    i=0
-    for _ in list_of_started_tasks:
+    t1 = time.perf_counter()  # Start timer
+    for i, j in enumerate(range(num), start=1):
+        image_obj = requests.get("https://loremflickr.com/3200/2400")  # Get image
+        task = threading.Thread(target=ImageDownloader, args=[image_obj, i])  # Create thread
+        task.start()  # Start thread
+        list_of_started_tasks.append(task)  # Add thread to list
 
-        list_of_started_tasks[i].join()
-        i+=1
-    
-    print('Time Taken to Download Image Files',time.perf_counter()-t1)
-    
-    str1="image download ended"
-    engine.say(str1)
+    # Wait for all threads to complete
+    for task in list_of_started_tasks:
+        task.join()
+
+    print('Time Taken to Download Image Files', time.perf_counter() - t1)
+    engine.say("Image download ended")
     engine.runAndWait()
 
-def VideoDownloader(url,n):
-    
-    text=requests.get(url)
-    if(not os.path.exists("DeleteME")):
-        os.mkdir("DeleteME")
-  
-    print(f"we are starting download {n+1}")
-    
-    file_Object=open(f'DeleteME/Video{n+1}.mp4','wb')
-    file_Object.write(text.content)
-    file_Object.close()
+# Function to download a video
+def VideoDownloader(url, n):
+    text = requests.get(url)
+    if not os.path.exists("DeleteME"):
+        os.mkdir("DeleteME")  # Create folder if not exists
 
-# PLEASE : check the validity of links before running
+    print(f"We are starting download {n+1}")
+    with open(f'DeleteME/Video{n+1}.mp4', 'wb') as file_Object:
+        file_Object.write(text.content)  # Save video content
 
+# Function to manage video downloads
 def Video_meta():
+    # List of video URLs
+    list_of_video = [
+        'https://download-video.akamaized.net/...1.mp4',
+        'https://vod-progressive.akamaized.net/...2.mp4',
+        'https://vod-progressive.akamaized.net/...3.mp4',
+    ]
 
-    list_of_video=['https://download-video.akamaized.net/v2-1/playback/29950fd4-8bbf-4d9e-ba91-1e967a5d92b2/e38749dc?__token__=st=1697219583~exp=1697233983~acl=%2Fv2-1%2Fplayback%2F29950fd4-8bbf-4d9e-ba91-1e967a5d92b2%2Fe38749dc%2A~hmac=635d10cfd220f20e8e62112e2a9bedcd57feb5c5f23d354cf95666806971a6b9&r=dXMtd2VzdDE%3D','https://vod-progressive.akamaized.net/exp=1697234039~acl=%2Fvimeo-transcode-storage-prod-us-east1-h264-1440p%2F01%2F4232%2F8%2F221163277%2F770983850.mp4~hmac=74d3693feed29942361ffef6515cf1794026b6c1cd5a59d942e0ccfc836c97d3/vimeo-transcode-storage-prod-us-east1-h264-1440p/01/4232/8/221163277/770983850.mp4?filename=file.mp4','https://vod-progressive.akamaized.net/exp=1697234082~acl=%2Fvimeo-transcode-storage-prod-us-central1-h264-1440p%2F01%2F2607%2F28%2F713036088%2F3305427178.mp4~hmac=2e84cbe6c4307358942650939b3bb484406bddbb267e73213b4d951abe580474/vimeo-transcode-storage-prod-us-central1-h264-1440p/01/2607/28/713036088/3305427178.mp4?filename=file.mp4','https://vod-progressive.akamaized.net/exp=1697234142~acl=%2Fvimeo-transcode-storage-prod-us-central1-h264-1440p%2F01%2F598%2F29%2F727993861%2F3375378558.mp4~hmac=03818fff26bf2e16e42592ffe6249d930200781d462625518cfa4d82c71f1cdb/vimeo-transcode-storage-prod-us-central1-h264-1440p/01/598/29/727993861/3375378558.mp4?filename=file.mp4','https://vod-progressive.akamaized.net/exp=1697234191~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3075%2F20%2F515379416%2F2390463005.mp4~hmac=36c496041e26f0bcad20a983d3e83bda47c87c8667447c8caf74a64ff8e32c4d/vimeo-prod-skyfire-std-us/01/3075/20/515379416/2390463005.mp4?filename=file.mp4']
-
-    str1=" Video download started"
-    engine.say(str1)
+    engine.say("Video download started")
     engine.runAndWait()
-    t1=time.perf_counter()
-   
+
+    t1 = time.perf_counter()  # Start timer
     with tpe() as executor:
-           executor.map(VideoDownloader,list_of_video,range(len(list_of_video)))
-    
-    print('Time Taken to Download Video Files',time.perf_counter()-t1)
-    
-    engine.say(str1:="video download end")
+        executor.map(VideoDownloader, list_of_video, range(len(list_of_video)))  # Parallel execution
+    print('Time Taken to Download Video Files', time.perf_counter() - t1)
+
+    engine.say("Video download ended")
     engine.runAndWait()
- 
 
+# Function to download a PDF
+def PdfDownloader(url, n):
+    text = requests.get(url)
+    if not os.path.exists("DeleteME"):
+        os.mkdir("DeleteME")  # Create folder if not exists
 
+    print(f"We are starting download {n+1}")
+    with open(f'DeleteME/PDF{n+1}.pdf', 'wb') as file_Object:
+        file_Object.write(text.content)  # Save PDF content
 
-def PdfDownloader(url,n):
-    
-    text=requests.get(url)
-    
-    if(not os.path.exists("DeleteME")):
-        os.mkdir("DeleteME")
-  
-    print(f"we are starting download {n+1}")
-    
-    with open(f'DeleteME/PDF{n+1}.pdf','wb') as file_Object:
-        file_Object.write(text.content)
-
-
+# Decorator function to add speech notifications for PDFs
 def decorator_For_pdf(PDF_meta):
-    
     def decofunc():
-        str1="PDF download started"
-        engine.say(str1)
+        engine.say("PDF download started")
         engine.runAndWait()
 
         PDF_meta()
 
-        engine.say(str1:="PDF download end")
+        engine.say("PDF download ended")
         engine.runAndWait()
-    
-    return decofunc
 
+    return decofunc
 
 @decorator_For_pdf
 def PDF_meta():
+    # List of PDF URLs
+    list_of_pdf = [
+        'http://dtu.ac.in/Web/notice/2023/oct/file1042.pdf',
+        'http://dtu.ac.in/Web/notice/2023/sep/file0928.pdf',
+    ]
 
-    list_of_pdf=['http://dtu.ac.in/Web/notice/2023/oct/file1042.pdf','http://dtu.ac.in/Web/notice/2023/sep/file0928.pdf','http://dtu.ac.in/Web/notice/2023/sep/file0920.pdf','http://dtu.ac.in/Web/notice/2023/sep/file09110.pdf']
-
-    t1=time.perf_counter()
-
+    t1 = time.perf_counter()  # Start timer
     with tpe() as executor:
-           executor.map(PdfDownloader,list_of_pdf,range(len(list_of_pdf)))
-    
-    print('Time Taken to Download PDF Files',time.perf_counter()-t1)
-    
-    
+        executor.map(PdfDownloader, list_of_pdf, range(len(list_of_pdf)))  # Parallel execution
+    print('Time Taken to Download PDF Files', time.perf_counter() - t1)
+
+# Function to sort downloaded files into respective folders
 def parallelsorting(file):
-
+    # Move files to appropriate folders based on extension
     if file.endswith('.jpg'):
-        shutil.move(f'DeleteMe/{file}',f'Sycamore/Image folder/{file}')
-   
-    if file.endswith('.mp4'):
-        shutil.move(f'DeleteMe/{file}',f'Sycamore/Video folder/{file}')
-    
-    if file.endswith('.pdf'):
-        shutil.move(f'DeleteMe/{file}',f'Sycamore/PDF folder')/{file}
+        shutil.move(f'DeleteMe/{file}', f'Sycamore/Image folder/{file}')
+    elif file.endswith('.mp4'):
+        shutil.move(f'DeleteMe/{file}', f'Sycamore/Video folder/{file}')
+    elif file.endswith('.pdf'):
+        shutil.move(f'DeleteMe/{file}', f'Sycamore/PDF folder/{file}')
 
+# Function to organize files into folders
 def file_sorting():
-   
-    List_of_files_in_DeleteMe=os.listdir('DeleteMe')
+    List_of_files_in_DeleteMe = os.listdir('DeleteMe')  # List all files
+    if not os.path.exists("Sycamore"):
+        os.mkdir("Sycamore")  # Create parent folder
 
-    if(not os.path.exists("Sycamore")):
-        os.mkdir("Sycamore")
-    
-    tup=('Image folder','Video folder','PDF folder')
-    
-    for i in tup:
-        
-        if(not os.path.exists(f"Sycamore/{i}")):
-            os.mkdir(f"Sycamore/{i}")
-    
-    engine.say(" file Sorting started")
+    # Create subfolders
+    for folder in ('Image folder', 'Video folder', 'PDF folder'):
+        if not os.path.exists(f"Sycamore/{folder}"):
+            os.mkdir(f"Sycamore/{folder}")
+
+    engine.say("File sorting started")
     engine.runAndWait()
 
-    t1=time.perf_counter()
-    print("Time taken in file sorting : ",time.perf_counter()-t1)
+    t1 = time.perf_counter()
     with tpe() as executor:
-        executor.map(parallelsorting,List_of_files_in_DeleteMe)
-    print("Time taken in file sorting : ",time.perf_counter()-t1)
-    
-    engine.say(str1:=" file Sorting ended")
+        executor.map(parallelsorting, List_of_files_in_DeleteMe)  # Parallel sorting
+    print("Time taken in file sorting: ", time.perf_counter() - t1)
+
+    engine.say("File sorting ended")
     engine.runAndWait()
 
+# Main logic for the script
+while number < 1:
+    number = 1
 
-while number<1:
-    number=1
-
-    name=input("Enter Your Name : ")
-    engine.say(f'{name} say:Start the execution')
+    name = input("Enter Your Name: ")
+    engine.say(f'{name}, say: Start the execution')
     engine.runAndWait()
+
     try:
         with sr.Microphone() as source2:
-            r.adjust_for_ambient_noise(source2,duration=0.1)
+            r.adjust_for_ambient_noise(source2, duration=0.1)  # Adjust for noise
             print("Wait")
             time.sleep(2)
-            print("Speak : \n\n\U0001F914\n\n")
-            audio2=r.listen(source2)
-            text=r.recognize_google(audio2)
-        
-            text.lower()
-            print('\tDid you say',text)
-            text.strip('')
-           
+            print("Speak:\n\n\U0001F914\n\n")
+            audio2 = r.listen(source2)  # Listen for audio
+            text = r.recognize_google(audio2).lower()  # Convert speech to text
+            print('\tDid you say', text)
     except:
-
-        print("Some Error has occured Please provide input in Text\n")
-        text=input("\nType :Start the execution")
-        text.lower()
-
+        print("Error occurred. Please provide input in text.")
+        text = input("\nType: Start the execution").lower()
     finally:
-        if __name__=='__main__':
-            
-            if text=='start the execution':
-            
-
+        if __name__ == '__main__':
+            if text == 'start the execution':
                 engine.say('Execution started')
                 engine.runAndWait()
 
-                Image_meta(10)  
-                #Video_meta()
-                PDF_meta()
-                file_sorting()
+                Image_meta(10)  # Download 10 images
+                # Video_meta()  # Uncomment to download videos
+                PDF_meta()  # Download PDFs
+                file_sorting()  # Sort files
 
-                
-                engine.say('work ended')
+                engine.say('Work ended')
                 engine.runAndWait()
 
                 time.sleep(60)
-                shutil.rmtree('DeleteMe')
-
+                shutil.rmtree('DeleteMe')  # Delete temporary folder
                 engine.say('DeleteMe folder deleted')
                 engine.runAndWait()
-
-
-
-
-
-
-
-
-
-
-
-
